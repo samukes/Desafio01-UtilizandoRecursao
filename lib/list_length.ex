9e14cdbf6aac63c7388d6673ab681a53f2dea473
samukes <darkdarumia@gmail.com>
2021-11-26 12:09:10 -0300

    Implementando um contador utilizando recursividade e tail call optmization(TCO)

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,9 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: sum(list, 0)
+
+  defp sum([], acc), do: acc
+
+  defp sum([_ | tail], acc) do
+    sum(tail, acc + 1)
   end
 end
